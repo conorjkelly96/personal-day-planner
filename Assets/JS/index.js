2f07a2c554a6c0dc528330d6adbc5e557b2c8194
Conor Kelly <conorjkelly96@gmail.com>
2021-10-11 21:55:56 +0100

    pushing CSS changes to git

---
+++ Assets/JS/index.js
@@ -0,0 +1,31 @@
+const onLoad = function () {
+  // on record load, check local storage
+  checkLocalStorage();
+
+  //Render Clock
+  renderClock();
+
+  //Render Hour Block
+  renderHourBlocks();
+
+  //set Present / Future color coordination
+  isPresent();
+};
+
+const checkLocalStorage = function () {
+  // function to check if anything is in local storage
+  // if so, return data
+  // if not, set default value
+};
+
+const renderClock = function () {
+  // function to render current day and time
+};
+
+const renderHourBlocks = function () {
+  // render hour blocks
+};
+
+const isPresent = function () {
+  // if present - set color to
+};
